2e57a6ebecc141efbd5d0be78277cd8e119670e8
AkimaRea <140754658+AkimaRea@users.noreply.github.com>
2023-10-08 09:30:35 +0300

    Update light_sensor.h

--- light_sensor.h
+++ light_sensor.h
@@ -1,30 +1,18 @@
-byte sensorPin = A0;
-// Присваиваем имя для цифрового значения аналогового входа A0
-// int округляет значения 
-int value = 0;
-void setup() {
-  // Пин 3 со светодиодом будет выходом (англ. «output»)
-  pinMode(ledPin, OUTPUT);
-  // Пин A0 с фоторезистором будет входом (англ. «input»)
-}
-void loop() {
-  // Считываем значение с фоторезистора на аналоговом входе A0
-  value = analogRead(sensorPin);
-  // value значение в диапозоне от 0 до 1023
-  // Полученные значения на аналоговом входе A0 делим на 4 
-  //чтобы уложиться в диопозон от 0 до 255
-  data = value / 4;
-  // Включаем светодиод с полученной ранее мощностью - от 0 до 255
-  analogWrite(ledPin, data);
-}
+#pragma once
+#include <Arduino.h>
 
 class DL {
-  public:
-  DL(int pin = 2) {
+public:
+  DL(int pin) {
     DLpin = pin;
   }
+  void printResult() {
+    // Считываем значение с фоторезистора на аналоговом входе A0
+    value = analogRead(DLpin);
+    Serial.println(DLpin);
+  };
 
-  private:
+private:
   int DLpin;
   int value = 0;
 };
